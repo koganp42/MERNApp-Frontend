3a2864f74b5318ecfdccc72e6f55eedfefebe1fb
rsuttles58 <robert.suttles58@gmail.com>
2020-01-10 14:54:04 -0600

    Added place components, routing for those, and a custom Button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from "react";
-import {BrowserRouter as Router,Route,Redirect,Switch} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
-import MainNavigation from './shared/components/Navigation/MainNavigation'
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 
 const App = () => {
   return (
     <Router>
-      <MainNavigation/>
+      <MainNavigation />
       <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places">
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
     </Router>
   );
